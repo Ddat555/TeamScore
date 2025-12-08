aec8995d6f707e26b3b978c4969ba4383dd9c742
Ddat555 <110767404+Ddat555@users.noreply.github.com>
2025-12-08 23:15:43 +0400

    addTestForTask3J2

---
+++ J3/src/main/java/org/example/task2/CarNumberGenerator.java
@@ -0,0 +1,10 @@
+package org.example.task2;
+
+import java.util.List;
+
+public class CarNumberGenerator implements ArrayGenerator<String>{
+    @Override
+    public List<String> generationArray(int length) {
+        return List.of();
+    }
+}
