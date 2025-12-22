20874493a8b77b4a2fee576302ceb6361829e226
Ddat555 <110767404+Ddat555@users.noreply.github.com>
2025-12-22 16:53:21 +0400

    fix test2J3

--- J3/src/test/java/ru/teamscore/task2/ArrayGeneratorTest.java
+++ J3/src/test/java/ru/teamscore/task2/ArrayGeneratorTest.java
@@ -5,27 +5,41 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.regex.Pattern;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayGeneratorTest {
 
 
     @Test
     void booleanGeneratorTest() throws JsonProcessingException {
-        BooleanGenerator booleanGenerator = new BooleanGenerator(new Random(100));
-        assertEquals("[true, true, false, true, true]", Arrays.toString(booleanGenerator.generationArray(5)));
+        BooleanGenerator booleanGenerator = new BooleanGenerator();
+        var arr = booleanGenerator.generationArray(5);
+        assertEquals(5, arr.length);
+        assertTrue(Arrays.asList(arr).contains(Boolean.TRUE) ||
+                Arrays.asList(arr).contains(Boolean.FALSE));
     }
 
     @Test
     void diceGeneratorTest() throws JsonProcessingException {
-        DiceGenerator diceGenerator = new DiceGenerator(new Random(100));
-        assertEquals("[2, 5, 5, 1, 2]", Arrays.toString(diceGenerator.generationArray(5)));
+        DiceGenerator diceGenerator = new DiceGenerator();
+        var arr = diceGenerator.generationArray(5);
+        assertEquals(5, arr.length);
+        for (var el: arr){
+            assertTrue(el >= 1 && el <= 6);
+        }
     }
 
     @Test
     void carNumberGeneratorTest() throws JsonProcessingException {
-        CarNumberGenerator carNumberGenerator = new CarNumberGenerator(new Random(100));
-        assertEquals("[K048KY, C833KP, P729CO, C938CX, A182KX]", Arrays.toString(carNumberGenerator.generationArray(5)));
+        CarNumberGenerator carNumberGenerator = new CarNumberGenerator();
+        var arr = carNumberGenerator.generationArray(5);
+        assertEquals(5, arr.length);
+        Pattern pattern = Pattern.compile("^[ABCYXPKOM]\\d{3}[ABCYXPKOM]{2}$");
+        for (String number : arr) {
+            assertTrue(pattern.matcher(number).matches(),
+                    "Неверный формат: " + number);
+        }
     }
 }
